0aba3551fbfb1233d4690ce30f4b5698c4f6091c
Paul Endaya <endaya.paul@gmail.com>
2025-02-03 08:23:39 +0300

    implemented the PlatformList component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,6 +14,7 @@ import { FaAndroid } from "react-icons/fa6";
 import { IoLogoAppleAppstore } from "react-icons/io5";
 import { SiAtari, SiCommodore, SiSega, SiWeb3Dotjs } from "react-icons/si";
 import CriticScore from "./CriticScore";
+import PlatformList from "./PlatformList";
 
 interface Props {
   game: Game;
@@ -106,59 +107,9 @@ const GameCard = ({ game }: Props) => {
         />
         <Card.Body gap="2">
           <div className="d-flex justify-content-between">
-            <div className="platforms-list d-flex flex-wrap gap-2">
-              {game.parent_platforms.map(({ platform }) => {
-                let platformIcon = null;
-                switch (platform.name) {
-                  case "PC":
-                    platformIcon = <FaWindows />;
-                    break;
-                  case "PlayStation":
-                    platformIcon = <FaPlaystation />;
-                    break;
-                  case "Xbox":
-                    platformIcon = <FaXbox />;
-                    break;
-                  case "iOS":
-                    platformIcon = <IoLogoAppleAppstore />;
-                    break;
-                  case "Android":
-                    platformIcon = <FaAndroid />;
-                    break;
-                  case "Apple Macintosh":
-                    platformIcon = <FaApple />;
-                    break;
-                  case "Linux":
-                    platformIcon = <FaLinux />;
-                    break;
-                  case "Nintendo":
-                    platformIcon = <BsNintendoSwitch />;
-                    break;
-                  case "Atari":
-                    platformIcon = <SiAtari />;
-                    break;
-                  case "Commodore / Amiga":
-                    platformIcon = <SiCommodore />;
-                    break;
-                  case "Sega":
-                    platformIcon = <SiSega />;
-                    break;
-                  case "3DO":
-                    platformIcon = <SiWeb3Dotjs />;
-                    break;
-                  case "Neo Geo":
-                    platformIcon = <FaNeos />;
-                    break;
-                  case "Web":
-                    platformIcon = <FaGlobe />;
-                    break;
-                  default:
-                    // Handle other platforms here
-                    break;
-                }
-                return <div key={platform.id}>{platformIcon}</div>;
-              })}
-            </div>
+            <PlatformList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
             <CriticScore metacritic={game.metacritic} />
           </div>
           <Card.Title>{game.name}</Card.Title>
---
+++ src/components/PlatformList.tsx
@@ -0,0 +1,78 @@
+import { Platform } from "@/services/game-service";
+import { BsNintendoSwitch } from "react-icons/bs";
+import {
+  FaApple,
+  FaGlobe,
+  FaLinux,
+  FaNeos,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { FaAndroid } from "react-icons/fa6";
+import { IoLogoAppleAppstore } from "react-icons/io5";
+import { SiAtari, SiCommodore, SiSega, SiWeb3Dotjs } from "react-icons/si";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformList = ({ platforms }: Props) => {
+  return (
+    <div className="platforms-list d-flex flex-wrap gap-2">
+      {platforms.map((platform) => {
+        let platformIcon = null;
+        switch (platform.name) {
+          case "PC":
+            platformIcon = <FaWindows />;
+            break;
+          case "PlayStation":
+            platformIcon = <FaPlaystation />;
+            break;
+          case "Xbox":
+            platformIcon = <FaXbox />;
+            break;
+          case "iOS":
+            platformIcon = <IoLogoAppleAppstore />;
+            break;
+          case "Android":
+            platformIcon = <FaAndroid />;
+            break;
+          case "Apple Macintosh":
+            platformIcon = <FaApple />;
+            break;
+          case "Linux":
+            platformIcon = <FaLinux />;
+            break;
+          case "Nintendo":
+            platformIcon = <BsNintendoSwitch />;
+            break;
+          case "Atari":
+            platformIcon = <SiAtari />;
+            break;
+          case "Commodore / Amiga":
+            platformIcon = <SiCommodore />;
+            break;
+          case "Sega":
+            platformIcon = <SiSega />;
+            break;
+          case "3DO":
+            platformIcon = <SiWeb3Dotjs />;
+            break;
+          case "Neo Geo":
+            platformIcon = <FaNeos />;
+            break;
+          case "Web":
+            platformIcon = <FaGlobe />;
+            break;
+          default:
+            // Handle other platforms here
+            break;
+        }
+        return <div key={platform.id}>{platformIcon}</div>;
+      })}
+    </div>
+  );
+};
+
+export default PlatformList;
