8ae0c8b08fcbaabb11fc0758df79d4201a55ec0f
Paul Endaya <paul@boxonvision.com>
2025-02-06 11:02:08 +0300

    fixed the Platforms filter to pass platform object

--- src/components/PlatformsFilter.tsx
+++ src/components/PlatformsFilter.tsx
@@ -1,16 +1,30 @@
-import { Platform } from "@/services/game-service";
 import {
   NativeSelectField,
   NativeSelectRoot,
 } from "@/components/ui/native-select";
 import usePlatforms from "@/hooks/usePlatforms";
+import { Platform } from "@/services/game-service";
 
-const PlatformsFilter = () => {
+
+interface Props {
+  selectedPlatform: Platform | null; // Change the type of selectedPlatform to Platform
+  onChange: (platform: Platform | null) => void; // Change the type of onChange to accept a Platform object
+}
+
+const PlatformsFilter = ({ selectedPlatform, onChange }: Props) => {
   const { data } = usePlatforms();
+
   return (
     <div>
       <NativeSelectRoot variant="subtle" key="subtle">
-        <NativeSelectField placeholder="Select Platform">
+        <NativeSelectField 
+          placeholder="Select Platform" 
+          value={selectedPlatform?.id} // Assuming selectedPlatform is an object with an id property
+          onChange={(e) => {
+            const selectedPlatformId = parseInt(e.currentTarget.value);
+            const selectedPlatform = data.find(platform => platform.id === selectedPlatformId);
+            onChange(selectedPlatform ? selectedPlatform : null);
+          }}>
           {data.map((platform) => (
             <option key={platform.id} value={platform.id}>
               {platform.name}
