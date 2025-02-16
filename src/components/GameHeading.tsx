ae267d2f5cffe389d50ecc84bc865ee37b62db24
Paul Endaya <endaya.paul@gmail.com>
2025-02-16 07:39:53 +0300

    converted dynamic heading to a component

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,12 @@
+import { GameQuery } from "../services/game-service";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`;
+  return <h1>{heading}</h1>;
+};
+
+export default GameHeading; 
