efd1074a7c3a56b9f9dc259a6cd8f22ac27846fe
Paul Endaya <endaya.paul@gmail.com>
2025-02-16 07:30:31 +0300

    refactor: placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,9 @@
 import { ImageDimension } from "./game-service";
+import placeholderImage from '/assets/elementor-placeholder-image.webp';
 
 
 const getCroppedImageUrl = (url: string, dimension: ImageDimension) => {
-    if (!url) return '';
+    if (!url) return placeholderImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + `crop/${dimension.width}/${dimension.height}/` + url.slice(index);
