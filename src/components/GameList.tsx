4bda196f443bcf3e7fc1962c13b557548bf38f64
Paul Endaya <endaya.paul@gmail.com>
2025-03-13 22:21:46 +0300

    implemented infinite scroll to GameList

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -3,7 +3,8 @@ import GameCard from "./GameCard";
 import GameCardLoader from "./GameCardLoader";
 import useLocalLoading from "@/hooks/useLocalLoading";
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
@@ -13,29 +14,44 @@ const GameList = ({ gameQuery }: Props) => {
   const { data, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage } =
     useGames(gameQuery);
   const localLoading = useLocalLoading(isLoading);
+
+  const fetchedGamesCount =
+    data?.pages.reduce((acc, page) => acc + page.results.length, 0) || 0; //use reduce to sum all pages in data.pages
+
   return (
     <>
-      <div className="game-grid">
-        {data?.pages.map(
-          (
-            page,
-            index // loop through pages of data returned from infinite query
-          ) => (
-            <React.Fragment key={index}>
-              {page.results.map((game) => (
-                <div key={game.id} className="game-item">
-                  {localLoading ? <GameCardLoader /> : <GameCard game={game} />}
-                </div>
-              ))}
-            </React.Fragment>
-          )
-        )}
-      </div>
-      {hasNextPage && (
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        next={() => fetchNextPage()} // fetchNextPage is a function from useGames hook
+        hasMore={!!hasNextPage} // use !! to convert undefined to boolean
+        loader={<Spinner />}
+      >
+        <div className="game-grid">
+          {data?.pages.map(
+            (
+              page,
+              index // loop through pages of data returned from infinite query
+            ) => (
+              <React.Fragment key={index}>
+                {page.results.map((game) => (
+                  <div key={game.id} className="game-item">
+                    {localLoading ? (
+                      <GameCardLoader />
+                    ) : (
+                      <GameCard game={game} />
+                    )}
+                  </div>
+                ))}
+              </React.Fragment>
+            )
+          )}
+        </div>
+      </InfiniteScroll>
+      {/* {hasNextPage && (
         <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
           {isFetchingNextPage ? "Loading..." : "Load More"}
         </Button>
-      )}
+      )} */}
     </>
   );
 };
